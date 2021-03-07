5b86bb2c1ebe2dd7eda3a3e0cf8b15d206ecc1f0
DyakonovAlex <happy-dyakonov@yandex.ru>
2021-03-07 20:53:14 +0300

    Changed init

--- 05/sorting/init.py
+++ 05/sorting/init.py
@@ -6,3 +6,15 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument("len", type=int, help="Length array N >= 0")
     parser.add_argument("array", type=str, help="Array like 1,2,3,4,5")
     return parser
+
+
+def get_array():
+    parser = init_argparse()
+    args = parser.parse_args()
+    arr_len: int = args.len
+    array: list = list(map(int, args.array.split(",")))
+    return array
+
+
+def print_result(arr):
+    print(",".join(str(x) for x in arr))
