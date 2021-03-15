f8639e8577402720019c74b0d23d87e293d4fd19
DyakonovAlex <happy-dyakonov@yandex.ru>
2021-03-15 13:40:39 +0300

    Added BST

---
+++ 07/tree/bst.py
@@ -0,0 +1,112 @@
+import logging
+
+from typing import Optional, List
+
+from node import Node
+
+
+class BST:
+    __slots__ = ('root',)
+
+    def __init__(self):
+        self.root = None
+
+    def get_root(self) -> Node:
+        return self.root
+
+    def insert(self, key) -> None:
+        if self.root:
+            self._insert(self.root, key)
+        else:
+            self.root = Node(key)
+
+    def _insert(self, node: Node, key: int) -> Node:
+        if node is None:
+            return Node(key)
+
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+
+        return node
+
+    def search(self, key) -> Node:
+        return self._search(self.root, key)
+
+    def _search(self, node: Node, key: int) -> Optional[Node]:
+        if node is None or node.key == key:
+            return node
+
+        if key < node.key:
+            node = self._search(node.left, key)
+        else:
+            node = self._search(node.right, key)
+        return node
+
+    def delete(self, key: int) -> None:
+        if self.root:
+            self._delete(self.root, key)
+
+    def _delete(self, node: Node, key: int) -> Optional[Node]:
+        if not node:
+            return None
+
+        if node.key == key:
+            if node.left:
+                left_right_most = node.left
+                while left_right_most.right:
+                    left_right_most = left_right_most.right
+                node.left = self._delete(node.left, left_right_most.key)
+                left_right_most.right = node.right
+                left_right_most.left = node.left
+                return left_right_most
+            else:
+                return node.right
+        elif key < node.key:
+            node.left = self._delete(node.left, key)
+        else:
+            node.right = self._delete(node.right, key)
+        return node
+
+    def inorder(self) -> List[int]:
+        return self._inorder(self.root)
+
+    def _inorder(self, node) -> List[int]:
+        arr = []
+        if node:
+            arr = self._inorder(node.left)
+            arr.append(node.key)
+            arr = arr + self._inorder(node.right)
+        return arr
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+    bst = BST()
+    arr = [20, 10, 30, 4, 16, 7, 5, 8, 14, 18, 11, 12]
+    for x in arr:
+        bst.insert(x)
+
+    print("=" * 100)
+    bst.get_root().display()
+
+    print("=" * 100)
+    print("in order:", *bst.inorder())
+
+    print("=" * 100)
+    print("search 16:", bst.search(16))
+    print("search 40:", bst.search(43))
+
+    print("=" * 100)
+    print("delete 10")
+    bst.delete(10)
+    print("in order:", *bst.inorder())
+
+    print("=" * 100)
+    bst.get_root().display()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 07/tree/node.py
@@ -0,0 +1,60 @@
+class Node:
+    __slots__ = ('left', 'right', 'key')
+
+    def __init__(self, key: int):
+        self.left = None
+        self.right = None
+        self.key = key
+
+    def __str__(self):
+        return str(self.key)
+
+    def display(self):
+        lines, *_ = self._display()
+        for line in lines:
+            print(line)
+
+    def _display(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display()
+            s = '%s' % self.key
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display()
+        right, m, q, y = self.right._display()
+        s = '%s' % self.key
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
